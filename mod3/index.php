81e6317e87e39418c79de80ae1c0bd8ac3dd1686
Anitta Siby <anittasiby002@gmail.com>
2023-10-27 11:31:56 +0530

    Add files via upload

---
+++ mod3/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $name = "Anitta";
+    $txt = "Web Programming using PHP";
+    echo "<center><b>I love $txt!</b></center>";
+    print("<h1><center>Welcome to PHP, $name!</center></h1>");
+    ?>
+</body>
+</html>
