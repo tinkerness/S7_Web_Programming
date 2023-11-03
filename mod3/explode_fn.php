64fc88fcb2602f7c4989804f2fbc431eedc3e470
Anitta Siby <anittasiby002@gmail.com>
2023-11-02 23:26:11 -0700

    Add files via upload

---
+++ mod3/explode_fn.php
@@ -0,0 +1,13 @@
+<?php
+echo ("Explode Funtion<br/>");
+$str = "Welcome to PHP World";
+$words = explode(" ",$str);
+
+foreach( $words as $myelement)
+echo $myelement."<br/>";
+echo "<br/>";
+
+echo ("Implode Funtion<br/>");
+$str2 = implode(" ",$words);
+echo $str2;
+?>
