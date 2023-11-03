38c2ad1c8cd7d6ccf3f337179d4f23fb947be2d5
Anitta Siby <anittasiby002@gmail.com>
2023-11-02 22:53:52 -0700

    Update palindrome.php

--- mod3/palindrome.php
+++ mod3/palindrome.php
@@ -1,38 +1,39 @@
 <?php
 if(isset($_POST['submit']))
 {
-        $num = $_POST['mynum'];
-        $x =0;
-        $n = $num;
-        while (floor($num)) 
-        {
-            $mod = $num%10;
-            $x = $x * 10 + $mod;
-            $num = $num/10;
-        }
-        if($n == $x)
-        {
-            echo " Pa;lindrome ";
-        }
-
-
-
-        
+    // number_format($num), (int)$num, intval($num)
+    $num = intval($_POST['mynum']);
+    $x =0;
+    $n = $num;
+    while (floor($num)) 
+    {
+        $mod = $num%10;
+        $x = $x * 10 + $mod;
+        $num = $num/10;
+    }
+    if($n == $x)
+    {
+        echo "$n is a Palindrome number.";
+    }
+    else
+    {
+        echo "$n is not a Palindrome number.";
+    }     
 }
 else
 {?>
-<!-- break php else block to render the html part -->
+<!-- html part -->
     <html>
-        <head><title>Hello Name</title></head>
+        <head><title>Palindrome</title></head>
         <body>
-            <form action="testhello.php" method="post">
+            <form action="palindrome.php" method="post">
                 Enter Num : <input type="text" name="mynum" />
                 <br/><br/>
                 <input type="submit" name="submit" value="Get Result" />
             </form>
         </body>
     </html>
-<!-- start php code again -->
+<!-- php part -->
 <?php
 }
 ?>
