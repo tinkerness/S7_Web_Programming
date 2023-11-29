6960916e363aaf5392452c129e43eaf3eec0312a
Anitta Siby <anittasiby002@gmail.com>
2023-11-30 01:49:40 +0530

    m5 json

---
+++ mod5/1_json/decode_fn.php
@@ -0,0 +1,13 @@
+<?php
+$jsonobj = '{
+    "name":"John",
+    "age":30,
+    "email":"john@gmail.com"
+}';
+$obj = json_decode($jsonobj);
+echo "<br/>Name : ".$obj->name."<br/>Age : ".$obj->age."<br/>Email : ".$obj->email."<br/><br/>";
+
+// php function
+var_dump($obj);
+
+?>
---
+++ mod5/1_json/php_object.php
@@ -0,0 +1,12 @@
+<?php
+class book {
+    public $title = "";
+    public $author = "";
+    public $yearofpublication = "";
+
+    $book = new Book();
+    $book->title = "WWW";
+    $book->author = "Ryn Gen";
+    $book->yearofpublication = "2019";
+}
+?>
